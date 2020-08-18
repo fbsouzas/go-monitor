1ef52d7eb8805701367fdfdc38dd3a549bb01e21
Fábio Souza <fabio.souza@vaipe.com.br>
2020-08-18 14:54:16 -0300

    Adds reader file

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
+
+const monitories = 3
+const delay = 5
 
 func main() {
 	showWelcomeMessage()
-	showMenu()
 
-	command := scanCommand()
+	for {
+		showMenu()
 
-	switch command {
-	case 1:
-		fmt.Println("Iniciando monitoramente")
-	case 2:
-		fmt.Println("Exigibindo logs")
-	case 0:
-		exitApp()
-	default:
-		fmt.Println("Comando não encontrado")
+		command := scanCommand()
+
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exigibindo logs")
+		case 0:
+			exitApp()
+		default:
+			fmt.Println("Comando não encontrado")
+		}
 	}
 }
 
@@ -50,4 +61,59 @@ func exitApp() {
 	fmt.Println("Saindo da aplicação")
 
 	os.Exit(0)
+}
+
+func startMonitoring() {
+	fmt.Println("Iniciando monitoramento...")
+
+	websites := readerFile()
+
+	for i := 0; i < monitories; i++ {
+		for _, website := range websites {
+			websiteTester(website)
+		}
+
+		time.Sleep(delay * time.Second)
+	}
+}
+
+func websiteTester(website string) {
+	resp, err := http.Get(website)
+	message := "foi carregado com sucesso!"
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode != 200 {
+		message = "está com problemas."
+	}
+
+	fmt.Println("O site", website, message, "Status code:", resp.StatusCode)
+}
+
+func readerFile() []string {
+	var websites []string
+
+	file, err := os.Open("websites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		websites = append(websites, line)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	file.Close()
+
+	return websites
 }
